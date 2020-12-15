858e9cca6a5393c546ae5c42b0ff19e7244b198b
田端佑也 <tabayu5@gmail.com>
2020-12-15 23:47:01 +0900

    first

--- lib/osenchi-stack.ts
+++ lib/osenchi-stack.ts
@@ -1,9 +1,16 @@
+import * as s3 from '@aws-cdk/aws-s3';
 import * as cdk from '@aws-cdk/core';
 
 export class OsenchiStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const inputBucket = new s3.Bucket(this, 'OsenchiInputBucket', {
+      bucketName: 'osenchi-inputbucket'
+    });
+
+    const outputBucket = new s3.Bucket(this, 'OsenchiOutputBucket', {
+      bucketName: 'osenchi-outputbucket'
+    });
   }
 }
